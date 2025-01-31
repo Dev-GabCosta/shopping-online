4588d978a32ef4695ec9043cf3d078bffa83ccf0
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-31 15:03:29 -0300

    fix(ProductMapperRequest): Corrigir método map para receber ProductRequest e retornar Product

--- src/main/java/com/online/shopping/mappers/ProductMapperRequest.java
+++ src/main/java/com/online/shopping/mappers/ProductMapperRequest.java
@@ -6,11 +6,11 @@ import com.online.shopping.models.Product;
 public class ProductMapperRequest implements GenericMapper<Product, ProductRequest> {
 
 	@Override
-	public ProductRequest map(Product entity) {
-		return new ProductRequest(
-				entity.getName(),
-				entity.getStock(),
-				entity.getPrice()
+	public Product map(ProductRequest request) {
+		return new Product(
+				request.name(),
+				request.stock(),
+				request.price()
 		);
 	}
 
