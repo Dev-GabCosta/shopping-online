b6e9b52a79f27096cd7805cbf25c20ed59fff78b
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-03 14:35:41 -0300

    feat(ProductRequest): Adicionar validação à requisição de produtos

--- src/main/java/com/online/shopping/dtos/ProductRequest.java
+++ src/main/java/com/online/shopping/dtos/ProductRequest.java
@@ -1,8 +1,12 @@
 package com.online.shopping.dtos;
 
+import jakarta.validation.constraints.Positive;
+
 public record ProductRequest (
 		String name,
+		@Positive(message = "O estoque deve ser maior que 0")
 		Integer stock,
+		@Positive(message = "O preço deve ser maior que 0")
 		Double price
 ){
 }
