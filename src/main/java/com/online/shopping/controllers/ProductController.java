8846ace3b22d58e4486ecb71b020ef4396a2a0e3
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-03 09:22:46 -0300

    feat(ProductController): Adicionar funcionalidades de listar produtos e deletar produto por id

--- src/main/java/com/online/shopping/controllers/ProductController.java
+++ src/main/java/com/online/shopping/controllers/ProductController.java
@@ -3,12 +3,12 @@ package com.online.shopping.controllers;
 import com.online.shopping.dtos.ProductRequest;
 import com.online.shopping.dtos.ProductResponse;
 import com.online.shopping.services.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -24,4 +24,17 @@ public class ProductController {
 		ProductResponse response = service.createProduct(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
+
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getAllProducts() {
+		List<ProductResponse> products = service.getAllProducts();
+		return ResponseEntity.status(HttpStatus.OK).body(products);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteProduct(@PathVariable Long id){
+		service.deleteProduct(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
 }
