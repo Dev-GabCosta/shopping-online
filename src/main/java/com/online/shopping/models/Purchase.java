69cca8963071638e72ee3942c0f34b7d15200f38
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-03 15:26:26 -0300

    refactor(Purchase): Adicionar campo id

--- src/main/java/com/online/shopping/models/Purchase.java
+++ src/main/java/com/online/shopping/models/Purchase.java
@@ -1,12 +1,18 @@
 package com.online.shopping.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Purchase {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private final String cpf;
 	private final List<Product> products;
 
