5648ed0580cc7623bbbcc1a1ff537690a643b3fd
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-04 16:36:08 -0300

    refactor(ProductRepository): Adicionar método para encontrar um produto por nome

--- src/main/java/com/online/shopping/repositories/ProductRepository.java
+++ src/main/java/com/online/shopping/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.online.shopping.repositories;
 
 import com.online.shopping.models.Product;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	Optional<Product> findByName(String name);
 }
