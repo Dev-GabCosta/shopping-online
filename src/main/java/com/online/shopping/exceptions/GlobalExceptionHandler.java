2795b7cf65881511bbdc56932b797681544071fa
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-05 16:24:17 -0300

    feat(GlobalExceptionHandler): Adicionar tratamento para o caso de um e-mail ou CPF inválido ser enviado na requisição

--- src/main/java/com/online/shopping/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/online/shopping/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,15 @@
 package com.online.shopping.exceptions;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -53,8 +52,20 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<Map<String, String>> handleDataIntegrityViolation(DataIntegrityViolationException exception) {
 		Map<String, String> error = new HashMap<>();
-error.put("Erro", "É possível que você esteja tentando enviar um valor (nome do produto ou CPF) já armazenado no banco de dados. Por favor, verifique e tente novamente");
+		error.put("Erro", "É possível que você esteja tentando enviar um valor (nome do produto ou CPF) já armazenado no banco de dados. Por favor, verifique e tente novamente");
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+		Map<String, String> errors = new HashMap<>();
+
+		for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+			String fieldName = fieldError.getField();
+			String fieldMessage = fieldError.getDefaultMessage();
+			errors.put(fieldName, fieldMessage);
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
 }
