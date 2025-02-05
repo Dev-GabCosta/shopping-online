2320c1133dfbe59bedfc0340dcefda04c114a2e2
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-05 09:22:03 -0300

    feat(CustomerRepository): Adiciona método para procurar por um cliente com base em seu  CPF

--- src/main/java/com/online/shopping/repositories/CustomerRepository.java
+++ src/main/java/com/online/shopping/repositories/CustomerRepository.java
@@ -1,8 +1,15 @@
 package com.online.shopping.repositories;
 
 import com.online.shopping.models.Customer;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.lang.ScopedValue;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	boolean existsByEmail(String email);
+
+	Optional<Customer> findByCpf(String cpf);
 }
