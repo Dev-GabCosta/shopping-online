cdb9a661c0c6e9803ce812158bfb9f8c908ba0d9
Gabriel Costa <gabriel.costa@zup.com.br>
2025-02-05 09:31:38 -0300

    feat(ProductRequest): Adicionar anotação para tornar obrigatória a inserção do nome de um produto

--- src/main/java/com/online/shopping/dtos/ProductRequest.java
+++ src/main/java/com/online/shopping/dtos/ProductRequest.java
@@ -1,8 +1,10 @@
 package com.online.shopping.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record ProductRequest (
+		@NotNull(message = "O nome do produto é obrigatório")
 		String name,
 		@Positive(message = "O estoque deve ser maior que 0")
 		Integer stock,
